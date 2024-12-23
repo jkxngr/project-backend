5cda2ef5920f3021b81e102649e1313ab2859c01
jkxngr <botirovj781@gmail.com>
2024-12-23 12:31:20 +0500

    bug fix

--- config/database.js
+++ config/database.js
@@ -9,8 +9,18 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
-        logging: false
+        logging: false,
+        dialectOptions: {
+            connectTimeout: 10000 // Set timeout for establishing a connection (10 seconds)
+        },
+        pool: {
+            max: 5,  // Max number of connection in pool
+            min: 0,  // Min number of connections in pool
+            acquire: 30000, // Time to wait before throwing an error when acquiring a connection
+            idle: 10000    // Max time before a connection is released when idle
+        }
     }
+    
 );
 
 module.exports = sequelize;
