3089e06bf0f09fb324aa27eaa19e1a3a7edea695
jkxngr <botirovj781@gmail.com>
2024-12-23 13:40:02 +0500

    bug fix

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.use(cors());
-app.use(bodyParser.json());
 app.use("/api", routes);
 
 console.log("Starting server...");
