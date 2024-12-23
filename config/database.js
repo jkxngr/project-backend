ab558cee65679b52793eef783a5225c785aee5a4
jkxngr <botirovj781@gmail.com>
2024-12-23 14:41:39 +0500

    bug fix

--- config/database.js
+++ config/database.js
@@ -1,26 +1,25 @@
-const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv');
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: false,
-        dialectOptions: {
-            connectTimeout: 20000 // Set timeout for establishing a connection (10 seconds)
-        },
-        pool: {
-            max: 5,  // Max number of connection in pool
-            min: 0,  // Min number of connections in pool
-            acquire: 30000, // Time to wait before throwing an error when acquiring a connection
-            idle: 10000    // Max time before a connection is released when idle
-        }
-    }
-    
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      connectTimeout: 20000,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
 );
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -7,24 +7,12 @@ const routes = require("./routes");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 8080; // Use 8080 as the default port
-
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 8080;
 app.use("/api", routes);
-
-console.log("Starting server...");
-console.log("Environment variables:", process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST);
 
 sequelize.sync({ force: false }).then(() => {
   console.log("Database synced.");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => {
   console.error("Failed to sync database:", err);
-  console.error("Error details:", {
-    name: err.name,
-    message: err.message,
-    stack: err.stack,
-    parent: err.parent
-  });
 });
