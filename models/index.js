8c9bd3702286729b195e0147eef643b95aff60e3
jkxngr <botirovj781@gmail.com>
2024-12-25 14:27:59 +0500

    auth

--- models/index.js
+++ models/index.js
@@ -9,4 +9,4 @@ Template.hasMany(Form, { foreignKey: "template_id" });
 Form.belongsTo(Template, { foreignKey: "template_id" });
 Topic.hasMany(Template, { foreignKey: "topic_id" });
 Template.belongsTo(Topic, { foreignKey: "topic_id" });
-module.exports = { sequelize, Template, Form , Topic };
+module.exports = { sequelize, Template, Form , Topic , User };
