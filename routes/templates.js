978e5eb9a81c04ef06d4efab3041a1eac4e5269f
jkxngr <botirovj781@gmail.com>
2025-01-02 17:39:54 +0500

    edit template

--- routes/templates.js
+++ routes/templates.js
@@ -1,62 +1,50 @@
 const express = require("express");
-const { Template, User, CheckboxOption } = require("../models");
+const { Template, User } = require("../models");
 const { authenticate, authorize } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const templates = await Template.findAll({
-    include: [
-      {
-        model: User,
-        attributes: ["name", "surname"],
-      },
-      {
-        model: CheckboxOption,
-      },
-    ],
+    include: {
+      model: User,
+      attributes: ["name", "surname"],
+    },
   });
   res.json(templates);
 });
-
+router.get("/:id", async (req, res) => {
+  const template = await Template.findByPk(req.params.id);
+  if (template) res.json(template);
+  else res.status(404).json({ error: "Template not found" });
+});
 router.post(
   "/",
   authenticate,
   authorize(["admin", "user"]),
   async (req, res) => {
+    const template = await Template.create(req.body);
+    res.status(201).json(template);
+  }
+);
+
+router.put(
+  "/:id",
+  authenticate,
+  authorize(["admin", "user"]),
+  async (req, res) => {
     try {
-      const { title, content, user_id, checkboxOptions } = req.body;
-      const template = await Template.create({ title, content, user_id });
-
-      if (checkboxOptions && checkboxOptions.length > 0) {
-        for (const option of checkboxOptions) {
-          await CheckboxOption.create({
-            template_id: template.template_id,
-            question_key: option.question_key,
-            option_text: option.option_text,
-          });
-        }
+      const template = await Template.findByPk(req.params.id);
+      if (template) {
+        await template.update(req.body);
+        res.json(template);
+      } else {
+        res.status(404).json({ error: "Template not found" });
       }
-
-      res.status(201).json(template);
     } catch (error) {
-      res.status(400).json({ error: "Template creation failed" });
+      res
+        .status(500)
+        .json({ error: "An error occurred while updating the template" });
     }
   }
 );
-router.get("/:id", async (req, res) => {
-  const template = await Template.findByPk(req.params.id, {
-    include: [
-      {
-        model: User,
-        attributes: ["name", "surname"],
-      },
-      {
-        model: CheckboxOption,
-      },
-    ],
-  });
-  if (template) res.json(template);
-  else res.status(404).json({ error: "Template not found" });
-});
-
 module.exports = router;
