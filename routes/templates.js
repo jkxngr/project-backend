0637edb0a742d31290d3c15205391c11feb80de5
jkxngr <botirovj781@gmail.com>
2025-01-03 14:17:07 +0500

    bug  fixes

--- routes/templates.js
+++ routes/templates.js
@@ -3,7 +3,7 @@ const { Template, User } = require("../models");
 const { authenticate, authorize } = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/", authenticate, async (req, res) => {
+router.get("/", async (req, res) => {
   const templates = await Template.findAll({
     include: {
       model: User,
@@ -12,36 +12,16 @@ router.get("/", authenticate, async (req, res) => {
   });
   res.json(templates);
 });
+
+router.post("/", authenticate, authorize(["admin", "user"]), async (req, res) => {
+  const template = await Template.create(req.body);
+  res.status(201).json(template);
+});
+
 router.get("/:id", async (req, res) => {
   const template = await Template.findByPk(req.params.id);
   if (template) res.json(template);
   else res.status(404).json({ error: "Template not found" });
 });
-router.post(
-  "/",
-  authenticate,
-  authorize(["admin", "user"]),
-  async (req, res) => {
-    try {
-      const { title, content, user_id, checkboxOptions } = req.body;
-      const template = await Template.create({ title, content, user_id });
 
-      if (checkboxOptions && checkboxOptions.length > 0) {
-        for (const option of checkboxOptions) {
-          await CheckboxOption.create({
-            template_id: template.template_id,
-            question_key: option.question_key,
-            option_text: option.option_text,
-          });
-        }
-      }
-
-      res.status(201).json(template);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "An error occurred while updating the template" });
-    }
-  }
-);
 module.exports = router;
