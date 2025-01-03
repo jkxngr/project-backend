144937d133c5fccc192a3b8f806d0a743c76f296
jkxngr <botirovj781@gmail.com>
2025-01-03 14:11:09 +0500

    Revert "edit template"
    This reverts commit 978e5eb9a81c04ef06d4efab3041a1eac4e5269f.

--- routes/templates.js
+++ routes/templates.js
@@ -1,83 +1,42 @@
 const express = require("express");
-const { Template, User, Like } = require("../models");
+const { Template, User } = require("../models");
 const { authenticate, authorize } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", authenticate, async (req, res) => {
   const templates = await Template.findAll({
-    include: [
-      {
-        model: User,
-        attributes: ["name", "surname"],
-      },
-      {
-        model: Like,
-        where: { userId: req.user.id },
-        required: false,
-        attributes: ["liked"],
-      },
-    ],
+    include: {
+      model: User,
+      attributes: ["name", "surname"],
+    },
   });
-  const templatesWithLikeStatus = templates.map((template) => ({
-    ...template.toJSON(),
-    likedByCurrentUser:
-      template.Likes.length > 0 ? template.Likes[0].liked : false,
-  }));
-
-  res.json(templatesWithLikeStatus);
+  res.json(templates);
 });
-
-router.get("/:id", authenticate, async (req, res) => {
-  const template = await Template.findByPk(req.params.id, {
-    include: [
-      {
-        model: User,
-        attributes: ["name", "surname"],
-      },
-      {
-        model: Like,
-        where: { userId: req.user.id },
-        required: false,
-        attributes: ["liked"],
-      },
-    ],
-  });
-
-  if (template) {
-    const templateWithLikeStatus = {
-      ...template.toJSON(),
-      likedByCurrentUser:
-        template.Likes.length > 0 ? template.Likes[0].liked : false,
-    };
-    res.json(templateWithLikeStatus);
-  } else {
-    res.status(404).json({ error: "Template not found" });
-  }
+router.get("/:id", async (req, res) => {
+  const template = await Template.findByPk(req.params.id);
+  if (template) res.json(template);
+  else res.status(404).json({ error: "Template not found" });
 });
-
 router.post(
   "/",
   authenticate,
   authorize(["admin", "user"]),
   async (req, res) => {
-    const template = await Template.create(req.body);
-    res.status(201).json(template);
-  }
-);
-
-router.put(
-  "/:id",
-  authenticate,
-  authorize(["admin", "user"]),
-  async (req, res) => {
     try {
-      const template = await Template.findByPk(req.params.id);
-      if (template) {
-        await template.update(req.body);
-        res.json(template);
-      } else {
-        res.status(404).json({ error: "Template not found" });
+      const { title, content, user_id, checkboxOptions } = req.body;
+      const template = await Template.create({ title, content, user_id });
+
+      if (checkboxOptions && checkboxOptions.length > 0) {
+        for (const option of checkboxOptions) {
+          await CheckboxOption.create({
+            template_id: template.template_id,
+            question_key: option.question_key,
+            option_text: option.option_text,
+          });
+        }
       }
+
+      res.status(201).json(template);
     } catch (error) {
       res
         .status(500)
@@ -85,36 +44,4 @@ router.put(
     }
   }
 );
-
-router.post("/:id/like", authenticate, async (req, res) => {
-  try {
-    const templateId = req.params.id;
-    const userId = req.user.id;
-    const existingLike = await Like.findOne({ where: { userId, templateId } });
-
-    if (existingLike) {
-      existingLike.liked = !existingLike.liked;
-      await existingLike.save();
-    } else {
-      await Like.create({ userId, templateId, liked: true });
-    }
-
-    const template = await Template.findByPk(templateId, {
-      include: [{ model: Like }],
-    });
-
-    const likesCount = template.Likes.filter((like) => like.liked).length;
-    template.likes = likesCount;
-    await template.save();
-
-    res
-      .status(200)
-      .json({ message: "Template like status updated successfully" });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while updating the like status" });
-  }
-});
-
 module.exports = router;
